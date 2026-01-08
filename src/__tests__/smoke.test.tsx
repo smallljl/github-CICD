ebd03b1534d2a4931be23c4813f9ef81f1ece02b
lijiaolong <168241296@qq.com>
2026-01-08 23:35:51 +0800

    feat:update

--- src/__tests__/smoke.test.tsx
+++ src/__tests__/smoke.test.tsx
@@ -1,11 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import { expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react'
+import { test, expect } from 'vitest'
 
-function Hello(){
-	return <h1>Hello Vite + React</h1>
+function Hello() {
+  return <h1>Hello Vite + React</h1>
 }
 
-test("renders", () => {
-	render(<Hello/>);
-	expect(screen.getByRole("heading", {name:"Hello Vite + React"})).toBeInTheDocument();
+test('renders', () => {
+  render(<Hello />)
+  expect(screen.getByRole('heading', { name: 'Hello Vite + React' })).toBeInTheDocument()
 })
