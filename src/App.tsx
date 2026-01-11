a56fe5a79ffe40ec31b08ece6f034b030f192efe
lijiaolong <168241296@qq.com>
2026-01-11 20:12:23 +0800

    feat:update

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React CICD 示例   我提交代码了 </h1>
+      <h1>Vite + React CICD 示例   我提交代码了 哈哈 </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
