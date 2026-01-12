a70a2a891d68d3d29cbd35847332c1042dedb470
lijiaolong <168241296@qq.com>
2026-01-12 21:50:08 +0800

    feat:update

--- src/__tests__/smoke.test.tsx
+++ src/__tests__/smoke.test.tsx
@@ -7,6 +7,7 @@ function Hello() {
 
 test('renders', () => {
   render(<Hello />)
+  // 这句话的意思是：期望页面上有一个 role 为 'heading'，名字为 'Hello Vite + React' 的元素，并且它确实存在于文档中
   expect(screen.getByRole('heading', { name: 'Hello Vite + React' })).toBeInTheDocument()
 })
 
